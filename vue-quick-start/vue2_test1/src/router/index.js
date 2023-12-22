9d0a27266e965a1d0c89e4ce1159f41f3d591d0c
Gorilla <1043638938@qq.com>
2023-12-22 18:13:42 +0800

    表单测试

--- vue-quick-start/vue2_test1/src/router/index.js
+++ vue-quick-start/vue2_test1/src/router/index.js
@@ -16,14 +16,14 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+        component: () => import('../views/AboutView.vue')
     }, {
         path: '/test',
         name: 'test',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/TestView.vue')
+        component: () => import('../views/TestView.vue')
     }
 ]
 
--- vue-quick-start/vue2_test1/src/views/TestView.vue
+++ vue-quick-start/vue2_test1/src/views/TestView.vue
@@ -1,14 +1,21 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="日期" width="180"/>
+    <el-table-column prop="name" label="姓名" width="180"/>
+    <el-table-column prop="address" label="地址"/>
+  </el-table>
+</template>
+
 <script>
 export default {
-  name: "TestView"
+  name: "TestView",
+  data() {
+    return {
+      tableData: []
+    }
+  }
 }
 </script>
-
-<template>
-  <div class="test">
-    <h1>This is a test page</h1>
-  </div>
-</template>
 
 <style scoped>
 
